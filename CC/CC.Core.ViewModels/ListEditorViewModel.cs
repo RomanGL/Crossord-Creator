36f815c07782dbcfc56bc4ecb947776d4e0ef738
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-04-14 19:43:33 +0600

    Правки верстки.

--- CC/CC.Core.ViewModels/ListEditorViewModel.cs
+++ CC/CC.Core.ViewModels/ListEditorViewModel.cs
@@ -6,24 +6,40 @@ using System.Threading.Tasks;
 using FirstFloor.ModernUI.Windows.Navigation;
 using CC.Core.Services.Interfaces;
 using Microsoft.Practices.Unity;
+using Prism.Commands;
+using PropertyChanged;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace CC.Core.ViewModels
 {
+    [ImplementPropertyChanged]
     public class ListEditorViewModel : ViewModelBase
     {
         public ListEditorViewModel(INavigationParametersResolver navigationParametersResolver)
         {
             _navigationParametersResolver = navigationParametersResolver;
+
+            SaveCommand = new DelegateCommand<bool?>(OnSaveCommand);
         }
 
-        [OptionalDependency("JsonListService")]
+        [DoNotNotify]
+        [Dependency("JsonListService")]
         public IListService JsonListService { get; set; }
 
-        [OptionalDependency("XmlListService")]
+        [DoNotNotify]
+        [Dependency("XmlListService")]
         public IListService XmlListService { get; set; }
+
+        [DoNotNotify]
+        public DelegateCommand<bool?> SaveCommand { get; private set; }
 
         public override void OnNavigatedTo(NavigationEventArgs e)
         {
+        }
+
+        private void OnSaveCommand(bool? isSaveAs)
+        {
+            ModernDialog.ShowMessage($"Save command invoked with parameter {isSaveAs}", "Crossword Creator", System.Windows.MessageBoxButton.OK);
         }
 
         private readonly INavigationParametersResolver _navigationParametersResolver;
